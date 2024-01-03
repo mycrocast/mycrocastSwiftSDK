ba209edbd7582198d6f843f22e7de945046f9e6e
sven-mycrocast <67374475+sven-mycrocast@users.noreply.github.com>
2024-01-03 12:15:19 +0100

    u

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/emqx/CocoaMQTT.git", exact: "2.1.3"),
+        .package(url: "https://github.com/mycrocast/MCCOpus", exact: "1.0.1")
     ],
     targets: [
         .binaryTarget(name: "MycrocastSDK", 
